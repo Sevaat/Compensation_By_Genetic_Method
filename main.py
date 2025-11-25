1216edd58b1db8781848f739157541f1164666e5
tkachenko <sevaatmail@gmail.com>
2025-11-25 20:52:04 +0500

    Добавлен расчет режима методом Ньютона

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from typing import List
 from src.classical_genetic_algorithm.classical_genetic_algorithm import CGA
+from src.newton_method.newton_method import NewtonMethod
 
 
-def main():
+def main_1():
     def user_function(param: List[str]):
         return sum([float(p) for p in param])
 
     cga = CGA(user_function)
     cga.run()
 
+def main_2():
+    nm = NewtonMethod()
+    nm.run()
+
 
 if __name__ == '__main__':
-    main()
+    main_2()
