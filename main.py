dd83fe90b4a839073a4fb64d95106a217c511e99
tkachenko <sevaatmail@gmail.com>
2025-11-25 20:39:48 +0500

    Добавлен классический генетический алгоритм

---
+++ main.py
@@ -0,0 +1,14 @@
+from typing import List
+from src.classical_genetic_algorithm.classical_genetic_algorithm import CGA
+
+
+def main():
+    def user_function(param: List[str]):
+        return sum([float(p) for p in param])
+
+    cga = CGA(user_function)
+    cga.run()
+
+
+if __name__ == '__main__':
+    main()
